d5f2d45930365622afc698fe2ffd046520f8596e
yanlp <85827444@qq.com>
2019-06-01 16:58:45 +0800

    Ribbon 核心组件IRule

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +19,13 @@ public class ConfigBean {//boot -->spring   applicationContext.xml --- @Configur
 
     @Bean
     @LoadBalanced // SpringCloud Ribbon是基于Netfix Ribbon实现的一套客户端,负载均衡的工具
-    public RestTemplate getRestTemplates() {
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public IRule getIRule() {
+        return new RandomRule();//达到的目的,用我们自己选择的算法,代替默认的轮训
+    }
+
 }
